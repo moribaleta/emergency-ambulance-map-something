046e46c255580aa03696f6120efe6f38356732bc
Unknown <moribaleta@gmail.com>
2018-02-21 06:29:29 +0800

    backup

---
+++ resources/json/hospitals.js
@@ -0,0 +1,136 @@
+var hospital_data = [
+    {
+        'name':'Garcia General Hospital',
+        'location':{
+            'lat':14.651058,
+            'long':121.110961,
+            'address': '49 Bayan-bayanan Ave. cor. T. Bugallon St. Marikina Heights, 1800, Marikina, Metro Manila, Philippines'
+        },
+        'available': 3,
+        'contact':'(02) 941 5511',
+        'onGoing':false
+    },
+    {
+        'name':'Marikina Valley Medical Center',
+        'location':{
+            'lat':14.634823,
+            'long':121.103887,
+            'address': '300 Shoe Ave, Sto. Nino, Marikina, 1800 Metro Manila'
+        },
+        'available': 3,
+        'contact':'(02) 682 2222',
+        'onGoing':false
+    },
+    {
+        'name':'Amang Rodriguez Memorial Medical Center',
+        'location':{
+            'lat':14.6358837,
+            'long':121.0982344,
+            'address': 'Sumulong Highway Sto. Nino, Marikina, 1800 Metro Manila'
+        },
+        'available': 3,
+        'contact': '(02) 941 5854',
+        'onGoing':false
+
+    },
+    {
+        'name':'St. Victoria Hospital',
+        'location':{
+            'lat':14.642167,
+            'long':121.094607,
+            'address': '444 JP Rizal, Sto Nino, Marikina, 1800 Metro Manila'
+        },
+        'available': 3,
+        'contact': '(02) 942 2022',
+        'onGoing':false
+
+    },
+    /* {
+        'name':'Manila Doctors Hospital',
+        'location':{
+            'lat':14.6355424,
+            'long':121.0990439,
+            'address': 'Blue Wave Strip Mall Marquinton Place, 22 Sumulong Highway Corner G. Fernando Ave, Sto. Nino, Marikina, 1800 Metro Manila'
+        },
+        'available': true,
+        'contact': '(02) 369 3976'
+
+    },*/
+    {
+        'name':'SDS Medical Center',
+        'location':{
+            'lat':14.639420, 
+            'long':121.109721,
+            'address': '61 Katipunan St, Marikina, 1800 Metro Manila'
+        },
+        'available': 4,
+        'contact': '(02) 933 1405',
+        'onGoing':false
+    },
+    {
+        'name':'San Ramon Hospital',
+        'location':{
+            lat: 14.644179, 
+            long:121.117118,
+            'address': '108 General Ordoñez Ave, Marikina, 1811 Metro Manila'
+        },
+        'available': 2,
+        'contact': '(02) 941 8632',
+        'onGoing':false
+    },
+    {
+        'name':'Immaculate Concepcion Hospital',
+        'location':{
+            'lat':14.6382441,
+            'long':121.1091564,
+            'address': '195 Katipunan St, Marikina, 1800 Metro Manila'
+        },
+        'available': 2,
+        'contact': '(02) 941 9362',
+        'onGoing':false
+    },
+    /*{
+        'name':'St. John The Baptist Hospital',
+        'location':{
+            'lat':14.6246366,
+            'long':121.0974502,
+            'address': 'Marikina, 1800 Metro Manila'
+        },
+        'available': 2,
+        'contact': '(02) 682 9093'
+    },*/
+    {
+        'name':'Salve Regina General Hospital, Inc',
+        'location':{
+            'lat':14.6199393,
+            'long':121.0961313,
+            'address': 'Marikina-Infanta Hwy, Pasig, 1800 Metro Manila'
+        },
+        'available': 3,
+        'contact': '(02) 477 4832',
+        'onGoing':false
+    },
+    {
+        'name':'St. Vincent General Hospital',
+        'location':{
+            'lat':14.650713,
+            'long':121.107741,
+            'address': '35 Bayan-Bayanan Ave, Marikina, 1800 Metro Manila'
+        },
+        'available': 3,
+        'contact': '(02) 369 7231',
+        'onGoing':false
+    },
+    {
+        'name':'St. Anthony Medical Center',
+        'location':{
+            'lat':14.624469,
+            'long':121.102825,
+            'address': '32 Santa Ana Ext., Marikina, 1801 Metro Manila'
+        },
+        'available': 3,
+        'contact': '(02) 369 7231',
+        'onGoing':false
+    },
+
+]
